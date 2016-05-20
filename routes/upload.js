436de7ba10f91701e7c63435a4a2292e0ef45838
Martin Deligny <martin.deligny@adeo.com>
2016-05-20 15:23:25 +0200

    remove aws s3 test for now

--- routes/upload.js
+++ routes/upload.js
@@ -26,52 +26,8 @@ var upload = multer({storage: storage, limits: limits}).any();
 router.post('/', function (req, res) {
   upload(req, res, function (err) {
 
-    // if unable to upload in local, then we'll upload it on aws s3 ...
     if (err) {
-
-      var file = req.files[0];
-
-      // first get the signed URL
-
-      aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
-
-      var s3 = new aws.S3();
-      var s3_params = {
-        Bucket: S3_BUCKET,
-        Key: file.filename,
-        Expires: 60,
-        ContentType: file.mimetype,
-        ACL: 'public-read'
-      };
-      s3.getSignedUrl('putObject', s3_params, function (err, data) {
-        if (err) {
-          console.log(err);
-          return res.end("Error uploading file.");
-        }
-        else {
-
-          var xhr = new XMLHttpRequest();
-          xhr.open("PUT", data);
-          xhr.setRequestHeader('x-amz-acl', 'public-read');
-          xhr.onload = function () {
-            if (xhr.status === 200) {
-              console.log('UPLOAD OK');
-              console.log('https://' + S3_BUCKET + '.s3.amazonaws.com/' + file.filename);
-              var response = {
-                filename: req.files[0].filename,
-                href: 'https://' + S3_BUCKET + '.s3.amazonaws.com/' + file.filename
-              };
-
-              res.send(response);
-            }
-          };
-          xhr.onerror = function () {
-            return res.end("Error uploading file.");
-          };
-          xhr.send(file);
-        }
-      });
-
+      return res.end("Error uploading file.");
     }
 
     var response = {
