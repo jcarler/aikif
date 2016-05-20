1654c0d6a102b64da00b06eb8c72af18f34783c0
Martin Deligny <martin.deligny@adeo.com>
2016-05-20 15:03:36 +0200

    working with AWS

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var multer = require('multer');
+var aws = require('aws-sdk');
 var router = express.Router();
+
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
 // CF Multer doc : https://github.com/expressjs/multer
 var storage = multer.diskStorage({
@@ -21,8 +26,52 @@ var upload = multer({storage: storage, limits: limits}).any();
 router.post('/', function (req, res) {
   upload(req, res, function (err) {
 
+    // if unable to upload in local, then we'll upload it on aws s3 ...
     if (err) {
-      return res.end("Error uploading file.");
+
+      var file = req.files[0];
+
+      // first get the signed URL
+
+      aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+
+      var s3 = new aws.S3();
+      var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: file.filename,
+        Expires: 60,
+        ContentType: file.mimetype,
+        ACL: 'public-read'
+      };
+      s3.getSignedUrl('putObject', s3_params, function (err, data) {
+        if (err) {
+          console.log(err);
+          return res.end("Error uploading file.");
+        }
+        else {
+
+          var xhr = new XMLHttpRequest();
+          xhr.open("PUT", data);
+          xhr.setRequestHeader('x-amz-acl', 'public-read');
+          xhr.onload = function () {
+            if (xhr.status === 200) {
+              console.log('UPLOAD OK');
+              console.log('https://' + S3_BUCKET + '.s3.amazonaws.com/' + file.filename);
+              var response = {
+                filename: req.files[0].filename,
+                href: 'https://' + S3_BUCKET + '.s3.amazonaws.com/' + file.filename
+              };
+
+              res.send(response);
+            }
+          };
+          xhr.onerror = function () {
+            return res.end("Error uploading file.");
+          };
+          xhr.send(file);
+        }
+      });
+
     }
 
     var response = {
