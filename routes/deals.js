35543af95e144ad661202a0b7c35cfee20e8e4e4
Martin Deligny <martin.deligny@adeo.com>
2016-05-20 13:39:25 +0200

    router, fileupload

---
+++ routes/deals.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var Deal = require('../models/deal');
+var mongoose = require('mongoose');
+
+var router = express.Router();
+
+mongoose.connect('mongodb://mooj:mooj@ds025792.mlab.com:25792/mooj');
+
+
+router.get('/',function (req, res) {
+  Deal.find(function (err, deals) {
+    if (err)
+      res.send(err);
+
+    res.json(deals);
+  });
+});
+
+router.post('/',function (req, res) {
+
+  var deal = new Deal();      // create a new instance of the Deal model
+  deal.name = req.body.name;
+  deal.description = req.body.description;
+
+  // save the bear and check for errors
+  deal.save(function (err) {
+    if (err)
+      res.send(err);
+
+    res.json({message: 'coucou ça marche!'});
+  });
+});
+
+module.exports = router;
---
+++ routes/upload.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var multer = require('multer');
+var router = express.Router();
+
+// CF Multer doc : https://github.com/expressjs/multer
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public/uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + file.originalname.substr(file.originalname.length - 15));
+  }
+});
+
+// limit file size to 5Mo
+var limits = {fileSize:500000};
+
+// Accept any files
+var upload = multer({storage: storage, limits: limits}).any();
+
+router.post('/', function (req, res) {
+  upload(req, res, function (err) {
+
+    if (err) {
+      return res.end("Error uploading file.");
+    }
+
+    var body = JSON.parse(req.body.imageUpload);
+
+    var response = {
+      href: body.href,
+      source: body.source,
+      modelCode: body.modelCode,
+      binaries: [
+        {
+          filename: req.files[0].filename,
+          format: 'standard',
+          href: req.protocol + '://' + req.get('host') + '/images/' + req.files[0].filename
+        }
+      ]
+    };
+
+    res.send(response);
+  })
+});
+
+module.exports = router;
