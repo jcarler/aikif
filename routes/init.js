76e7f613711b1fbe3d59fa2ad8e48982782bec69
Martin Deligny <martin.deligny@adeo.com>
2016-08-08 13:34:11 +0200

    test init file

--- routes/init.js
+++ routes/init.js
@@ -2,40 +2,45 @@ var express = require('express');
 var Merchant = require('../models/merchant');
 var Deal = require('../models/deal');
 
+var dealsFile = require('../dataInject/deals.json');
+
 
 var router = express.Router();
 
 
 router.get('/', function (req, res) {
-  var date = new Date();
 
-  Merchant
-    .find({moojPhone: '0366970527'})
-    .exec(function (err, merchant) {
-      if (err) {
-        console.log(err);
-        res.json(
-          {
-            message: 'Merchant not found'
-          }
-        );
-        res.status(404).end();
-      }
+  dealsFile.forEach(function(dealInjected) {
+    Merchant
+      .find({moojPhone: dealInjected.moojPhone})
+      .exec(function (err, merchant) {
+        if (err) {
+          console.log(err);
+          res.json(
+            {
+              message: 'Merchant not found'
+            }
+          );
+          res.status(404).end();
+        }
 
-      var deal = new Deal();      // create a new instance of the Deal model
-      deal.name = 'Test inject';
-      deal.description = 'Description';
-      deal.timestamp = date.getTime();
-      deal.merchant = merchant[0]._id;
+        var date = new Date();
+        var deal = new Deal();      // create a new instance of the Deal model
+        deal.name = dealInjected.name;
+        deal.description = dealInjected.description;
+        deal.timestamp = date.getTime();
+        deal.merchant = merchant[0]._id;
 
-      // save the bear and check for errors
-      deal.save(function (err) {
-        if (err)
-          res.send(err);
+        // save the bear and check for errors
+        deal.save(function (err) {
+          if (err)
+            res.send(err);
 
-        res.json({message: 'Deal created'});
+        });
       });
-    });
+  });
+
+  res.json({message: 'Init OK'});
 });
 
 module.exports = router;
