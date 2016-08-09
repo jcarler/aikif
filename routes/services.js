caf4a151e6ddc02df2de0d10cbfb107d48332066
Martin Deligny <martin.deligny@adeo.com>
2016-08-09 13:23:48 +0200

    add sms test

--- routes/services.js
+++ routes/services.js
@@ -3,7 +3,39 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/sms', function(req, res, next) {
-  res.send('coucou');
+  res.send('sms sent');
+  console.log(req.body);
+
+  var date = new Date();
+
+  Merchant
+    .find({moojPhone: '0366970527'})
+    .exec(function (err, merchant) {
+      if (err) {
+        console.log(err);
+        res.json(
+          {
+            message: 'Merchant not found'
+          }
+        );
+        res.status(404).end();
+      }
+
+      var deal = new Deal();      // create a new instance of the Deal model
+      deal.name = 'Test deal by sms';
+      deal.description = 'Sms sent from:' + req.body.From + ', body: ' + req.body.Body ;
+      deal.timestamp = date.getTime();
+      deal.merchant = merchant[0]._id;
+
+      // save the bear and check for errors
+      deal.save(function (err) {
+        if (err)
+          res.send(err);
+
+        res.json({message: 'Deal created'});
+      });
+    });
+
 });
 
 module.exports = router;
