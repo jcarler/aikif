fc90e1ad34471cedd30f8ab660b946b6fe694a1b
Martin Deligny <martin.deligny@adeo.com>
2016-09-08 16:49:03 +0200

    Feat: remove merchant account by sms

--- routes/services.js
+++ routes/services.js
@@ -8,63 +8,75 @@ var Deal = require('../models/deal');
 /* GET home page. */
 router.post('/sms', function (req, res, next) {
   var date = new Date();
+  var sms = req.body.Body;
 
-  Merchant
-    .find({moojPhone: req.body.From})
-    .exec(function (err, merchant) {
-      if (err) {
-        console.log(err);
-        res.json(
-          {
-            message: 'Merchant not found'
-          }
-        );
-        res.status(404).end();
-      }
+  if (sms.toLowerCase().indexOf('#supprimercompte#') >= 0) {
+    Merchant
+      .findAndRemove({moojPhone: req.body.From})
+      .exec(function (err) {
+        if (err)
+          res.send(err);
 
-      var sms = req.body.Body;
+        res.status(200).end();
+      });
+  }
+  else {
+    Merchant
+      .find({moojPhone: req.body.From})
+      .exec(function (err, merchant) {
+        if (err) {
+          console.log(err);
+          res.json(
+            {
+              message: 'Merchant not found'
+            }
+          );
+          res.status(404).end();
+        }
 
-      // Supprimer le dernier deal
-      if (sms.toLowerCase().indexOf('#supprimerdernier#') >= 0) {
-        Deal
-          .findOneAndRemove({
-            merchant: mongoose.Types.ObjectId(merchant[0]._id)
-          })
-          .sort({timestamp: -1})
-          .exec(function (err) {
+        // Supprimer le dernier deal
+        if (sms.toLowerCase().indexOf('#supprimerdernier#') >= 0) {
+          Deal
+            .findOneAndRemove({
+              merchant: mongoose.Types.ObjectId(merchant[0]._id)
+            })
+            .sort({timestamp: -1})
+            .exec(function (err) {
+              if (err)
+                res.send(err);
+
+              res.status(200).end();
+            });
+        }
+        // Supprimer tous les deals
+        else if (sms.toLowerCase().indexOf('#supprimertous#') >= 0) {
+          Deal
+            .findAndRemove({
+              merchant: mongoose.Types.ObjectId(merchant[0]._id)
+            })
+            .exec(function (err) {
+              if (err)
+                res.send(err);
+
+              res.status(200).end();
+            });
+        }
+        else {
+          var deal = new Deal();      // create a new instance of the Deal model
+          deal.description = sms;
+          deal.timestamp = date.getTime();
+          deal.merchant = merchant[0]._id;
+
+          // save the deal and check for errors
+          deal.save(function (err) {
             if (err)
               res.send(err);
 
             res.status(200).end();
           });
-      }
-      else if (sms.toLowerCase().indexOf('#supprimertous#') >= 0) {
-        Deal
-          .findAndRemove({
-            merchant: mongoose.Types.ObjectId(merchant[0]._id)
-          })
-          .exec(function (err) {
-            if (err)
-              res.send(err);
-
-            res.status(200).end();
-          });
-      }
-      else {
-        var deal = new Deal();      // create a new instance of the Deal model
-        deal.description = sms;
-        deal.timestamp = date.getTime();
-        deal.merchant = merchant[0]._id;
-
-        // save the bear and check for errors
-        deal.save(function (err) {
-          if (err)
-            res.send(err);
-
-          res.status(200).end();
-        });
-      }
-    });
+        }
+      });
+  }
 
 });
 
