7587dff6e9007fa0422a36c460eafd9792475dd1
Martin Deligny <martin.deligny@adeo.com>
2016-09-09 17:48:48 +0200

    chore: enable PUT

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'jade');
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST, GET");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.header("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization");
   next();
 });
