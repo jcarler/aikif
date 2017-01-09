4433a153c6f2a00b0147b653b9b24c6ec07ba512
Martin Deligny <martin.deligny@adeo.com>
2017-01-09 13:50:02 +0100

    fix: add anti date for init

--- routes/init.js
+++ routes/init.js
@@ -10,10 +10,15 @@ var router = express.Router();
 
 
 router.get('/', function (req, res) {
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth();
+  var year = date.getFullYear();
+  var antiDate = new Date(year, month, day);
 
   dealsFile.forEach(function (dealInjected) {
-    var date = new Date();
-    var timestamp = timestamp = date.getTime() - dealInjected.timestampGap;
+
+    var timestamp = antiDate.getTime() - dealInjected.timestampGap;
 
     dealService.createDeal(dealInjected.moojPhone, dealInjected.description, timestamp)
       .then(function () {
