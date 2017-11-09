5f5a646614b3a5bf9de4df442f76d3781ec6947a
Martin Deligny <martin.deligny@adeo.com>
2017-11-09 12:42:16 +0100

    fix conf file for database access

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 
 module.exports = {
   'secret': 'maintenantoujamaissecret',
-  'database': 'mongodb://mooj:mooj@ds149535.mlab.com:49535/aikif'
+  'database': 'mongodb://aikif:aikif@ds149535.mlab.com:49535/aikif'
 };
